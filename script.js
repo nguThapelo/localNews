ca69c5a8a20bbc221d30ad97313fe8db7e19d92e
Thapelo Mmakola <mthapelothapza@gmail.com>
2022-05-08 11:34:16 +0200

    aable to render different categories of news

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ const entertainmentBtn = document.getElementById('entertainment');
 const technologyBtn = document.getElementById('technology');
 const searchBtn = document.getElementById('searchBtn');
 
+window.onload = () => {
+    newstype.innerHTML = '<h4>Headlines</h4>';
+    fetchHeadlinesNews();
+};
+
 //input bar
 const newsinput = document.getElementById('newsinput');
 
@@ -31,30 +36,54 @@ const SEARCH_NEWS = 'https://newsapi.org/v2/everything?q=';
 //event listeners
 
 generalBtn.addEventListener('click', () => {
+    newstype.innerHTML = '<h4>General News</h4>';
+    
     fetchGeneralNews();
 });
 
 sportsBtn.addEventListener('click', () => {
+    newstype.innerHTML = '<h4>Sports News</h4>';
+
     fetchSportsNews();
 });
 
 businessBtn.addEventListener('click', () => {
+    newstype.innerHTML = '<h4>Business News</h4>';
+
     fetchBusinessNews();
 });
 
 entertainmentBtn.addEventListener('click', () => {
+    newstype.innerHTML = '<h4>Entertainment News</h4>';
+
     fetchEntertainmentNews();
 });
 
 technologyBtn.addEventListener('click', () => {
+    newstype.innerHTML = '<h4>Technology News</h4>';
+
     fetchTechnologyNews();
 });
 
 searchBtn.addEventListener('click', () => {
+    newstype.innerHTML = `<h4>Search: ${newsinput.value}</h4>`;
+
     fetchNews();
 });
 
+const fetchHeadlinesNews = async () => {
+    const res = await fetch(HEADLINES + API_KEY)
+    newsData = [];
+    if (res.status >= 200 && res.status < 300) {
+        const data = await res.json();
+        newsData = data.articles;
 
+    } else {
+        //handle error
+        console.log(res.status, res.statusText);
+    }
+    displayNews();
+}
 
 const fetchGeneralNews = async () => {
     const res = await fetch(GENERAL_NEWS + API_KEY)
@@ -153,12 +182,15 @@ const fetchNews = async () => {
     displayNews();
 };
 
+//displaying the news
+
 const displayNews = () => {
     newsdetails.innerHTML = '';
 if(newsData.length === 0) {
     newsdetails.innerHTML = '<h6>Error, no information found</h6>'
 }
 
+//loop through array and create elements to render data
 newsData.forEach(news => {
     var column = document.createElement('div');
     column.className='col-sm-12 col-md-4 col-lg-3 p-2 card';
@@ -173,6 +205,37 @@ newsData.forEach(news => {
 
     var cardBody = document.createElement('div');
 
+    var newsHeading = document.createElement('h5');
+    newsHeading.className = 'card-title';
+    newsHeading.innerHTML = news.title;
+
+    var dateHeading = document.createElement('h6');
+    dateHeading.className = 'text-primary';
+    const date = new Date();
+    dateHeading.innerHTML = date;
+
+    var description = document.createElement('p');
+    description.className = 'text-muted';
+    description.innerHTML = news.description;
     
+//Read more link
+    var link = document.createElement('a');
+    link.className = 'btn btn-dark';
+    link.setAttribute('target', '_blank');
+    link.href = news.url;
+    link.innerHTML = 'Read More';
+
+    cardBody.appendChild(newsHeading);
+    cardBody.appendChild(dateHeading);
+    cardBody.appendChild(description);
+    cardBody.appendChild(link);
+    
+    card.appendChild(image);
+    card.appendChild(cardBody);
+
+    column.appendChild(card);
+
+    newsdetails.appendChild(column);
+
 })
 }
